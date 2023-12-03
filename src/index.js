cc8dad1627a02b6adb69b08ab251f6c523774621
kunal <kunalkarne2020@gmail.com>
2023-12-03 14:53:24 +0530

    setup backend

---
+++ src/index.js
@@ -0,0 +1,76 @@
+// require('dotenv').config({path: './env'});
+import dotenv from 'dotenv';
+import connectDB from './db/db.js';
+import { app } from './app.js';
+
+dotenv.config({
+    path:'./.env'
+})
+
+
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+
+    app.on('error', (err)=>{
+        console.log('ERROR: ', err);
+        throw err
+    })
+   
+})
+.catch((err)=>{
+    console.log('MONGO db connection failed' , err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from 'express';
+const app = express();
+
+( async () =>{
+    try {
+        await mogoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on('error' , (error)=>{
+            console.log("ERROR: " , error);
+            throw error
+        })
+
+        app.listen(process.env.PORT , ()=>{
+            console.log(`App on listenig on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("ERROR: " , error);
+        throw error
+    }
+})()
+
+*/
